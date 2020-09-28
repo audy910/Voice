e831e5047f6cb990d5c1f3b7d14cba054f554e86
Audrey Reinhard <audianreinhard@gmail.com>
2020-09-28 14:53:42 -0700

    first commit
    drawable sidebar
    icons
    profile pic

---
+++ app/src/main/java/com/example/voice/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.voice;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView postList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                UserMenuSelector(item);
+                return false;
+            }
+
+            private void UserMenuSelector(MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.nav_profile:
+                       // Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+    }
+}
